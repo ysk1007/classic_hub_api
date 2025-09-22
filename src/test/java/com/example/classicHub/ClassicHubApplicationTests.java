82f0b6eddfcb722ade4d9ca0f975ff80b0c92924
okonys <yoonsg1007@gmail.com>
2025-09-22 14:39:26 +0900

    기본 프로젝트 생성

---
+++ src/test/java/com/example/classicHub/ClassicHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.classicHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassicHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
